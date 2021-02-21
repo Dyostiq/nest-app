ed605b3813e24fd8451e568406fcb2d4b1acb53c
Dominik Ostrowski <dominik.ostrowski@live.com>
2021-02-21 23:52:53 +0100

    chore: clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
 import {MongooseModule} from '@nestjs/mongoose';
+import {ConfigModule} from '@nestjs/config';
 import {UsersModule} from './users';
 import {AuthModule} from './auth';
-import {ConfigModule} from '@nestjs/config';
 
 @Module({
     imports: [
@@ -13,8 +11,8 @@ import {ConfigModule} from '@nestjs/config';
         AuthModule,
         ConfigModule.forRoot()
     ],
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {
 }
